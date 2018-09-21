c6aaffa6ca2eaf606cb0310a9ec53f29a10338cc
simarc86 <simarc86@gmail.com>
2018-09-21 16:56:02 +0200

    Initial Commit

---
+++ GnomesBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GnomesBrowser
+//
+//  Created by Marc Tamarit Romero on 21/9/18.
+//  Copyright © 2018 Marc Tamarit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
